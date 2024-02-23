1362db8ba9353f8a96dbfb6970cf4505c9e25b2e
wltjsdud0782 <wltjsdud0782@gmail.com>
2024-02-23 11:27:29 +0900

    main

---
+++ src/test/java/com/green/StudyRoom/StudyRoomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.green.StudyRoom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class StudyRoomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
