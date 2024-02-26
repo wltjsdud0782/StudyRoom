d3685d1121336caeacf24e71483ad636fdfb1fb7
SangHyeokY <ush529@gmail.com>
2024-02-26 16:48:31 +0900

    ysh0226last

--- src/main/java/com/green/StudyRoom/admin/controller/AdminController.java
+++ src/main/java/com/green/StudyRoom/admin/controller/AdminController.java
@@ -2,6 +2,8 @@ package com.green.StudyRoom.admin.controller;
 
 import com.green.StudyRoom.admin.service.AdminServiceImpl;
 import com.green.StudyRoom.admin.vo.MessageVO;
+import com.green.StudyRoom.member.service.MemberServiceImpl;
+import com.green.StudyRoom.seat.service.SeatServiceImpl;
 import jakarta.annotation.Resource;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -11,21 +13,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping("/admin")
 public class AdminController {
-
     @Resource(name="adminService")
     private AdminServiceImpl adminService;
+//    @Resource(name="memberService")
+//    private MemberServiceImpl memberService;
+//    @Resource(name="seatService")
+//    private SeatServiceImpl seatService;
 
     //(1)////////////////////////////////////////////////////// //
-    @GetMapping("/home")
-    public String adminHome(){
-        return "content/admin_content";
+    @GetMapping("/message")
+    public String adminMessage(){
+        return "content/admin_message_content";
     }
 
     @PostMapping("/sendMessage")
     public String sendMessage(MessageVO messageVO){
         adminService.insertMessage(messageVO);
         System.out.println(messageVO);
-        return "redirect:/admin/home";
+        return "redirect:/admin/message";
     }
     // ////////////////////////////////////////////////////// //
 }
