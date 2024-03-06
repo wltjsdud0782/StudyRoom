215edfd3c3c545ed126b83f5e01e352de249c33a
SangHyeokY <ush529@gmail.com>
2024-03-06 16:33:26 +0900

    ysh0433

---
+++ src/main/java/com/green/StudyRoom/admin/service/MessageServiceImpl.java
@@ -0,0 +1,47 @@
+package com.green.StudyRoom.admin.service;
+
+import com.green.StudyRoom.admin.vo.InfoSearchVO;
+import com.green.StudyRoom.admin.vo.MessageVO;
+import com.green.StudyRoom.member.vo.MemberVO;
+import org.mybatis.spring.SqlSessionTemplate;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service("messageService")
+public class MessageServiceImpl implements MessageService{
+
+    @Autowired
+    private SqlSessionTemplate sqlSession;
+
+    //채팅보낼 사람 조회
+    @Override
+    public List<MemberVO> selectWho(InfoSearchVO infoSearchVO) {
+        return sqlSession.selectList("messageMapper.selectWho", infoSearchVO);
+    }
+
+    //채팅받을 사람 (비동기)
+    @Override
+    public MemberVO selectMan(int memberCode) {
+        return sqlSession.selectOne("messageMapper.selectMan", memberCode);
+    }
+
+    //관리자 메세지 보내기
+    @Override
+    public void insertAdmMsg(MessageVO messageVO) {
+        sqlSession.insert("messageMapper.insertAdmMsg", messageVO);
+    }
+
+    //유저 메세지 저장
+    @Override
+    public void insertMessage(MessageVO messageVO) {
+        sqlSession.insert("messageMapper.insertMessage", messageVO);
+    }
+
+    //모든 메세지 조회
+    @Override
+    public List<MessageVO> selectMessage() {
+        return sqlSession.selectList("messageMapper.selectMessage");
+    }
+}
