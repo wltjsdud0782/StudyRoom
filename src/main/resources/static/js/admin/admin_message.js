0d99f975329a37ae08d5c0a9ce5ff7cf6409f9d2
SangHyeokY <ush529@gmail.com>
2024-03-27 14:46:12 +0900

    ysh0253

--- src/main/resources/static/js/admin/admin_message.js
+++ src/main/resources/static/js/admin/admin_message.js
@@ -34,19 +34,42 @@ function goChat(memberCode) {
         })
         //fetch 통신 후 실행 영역
         .then((data) => {//data -> controller에서 리턴되는 데이터!
-            console.log(data);
-
             document.querySelector('input[name="memberCode"]').value = memberCode;
             document.querySelector('#send_memberName').value = `@${data.member.memberName}`;
 
-            //여기에서 1:1 채팅창을 띄워주기
-            // const oneByone = document.querySelector('adminContainer-table-tbody2');
-            //임시
-            // oneByone.insertAdjacentHTML = '';
-            
-
-
-
+            const oneByone = document.querySelector('.adminContainer-table-tbody2');
+            oneByone.innerHTML = '';
+            let str = '';
+            data.chtList.forEach(e => {
+                str += `
+            <tr>
+                <td>
+                    <div class="userMsg">
+                        <span>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="16"
+                                height="16" fill="currentColor"
+                                class="bi bi-arrow-return-right" viewBox="0 0 16 16">
+                                <path fill-rule="evenodd"
+                                d="M1.5 1.5A.5.5 0 0 0 1 2v4.8a2.5 2.5 0 0 0 2.5 2.5h9.793l-3.347 3.346a.5.5 0 0 0 .708.708l4.2-4.2a.5.5 0 0 0 0-.708l-4-4a.5.5 0 0 0-.708.708L13.293 8.3H3.5A1.5 1.5 0 0 1 2 6.8V2a.5.5 0 0 0-.5-.5" />
+                            </svg>
+                        </span>
+                        <th:block th:if="${toFrom == 'TO'}" >
+                            <span>&nbsp;[[${chat.memberVO.memberName}]]([[${chat.memberVO.memberId}]]) 님에게</span>
+                        </th:block>
+                        <th:block th:else="${toFrom == 'TO'}" >
+                            <span>&nbsp;[[${chat.memberVO.memberName}]]([[${chat.memberVO.memberId}]]) 님으로부터</span>
+                        </th:block>
+                    </div>
+                    <div class="botMsg">
+                        <span>${e.messageContent}</span>
+                    </div>
+                    <div class="topMsg text-end">
+                        <span>${e.messageDate}&ensp;</span>
+                    </div>
+                </td>
+            </tr>`});
+            //th:each="chat : ${data.chtList}"
+            oneByone.insertAdjacentHTML('afterbegin', str)
         })
 
         //fetch 통신 실패 시 실행 영역
