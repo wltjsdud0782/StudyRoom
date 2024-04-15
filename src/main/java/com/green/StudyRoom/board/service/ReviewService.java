d708012476de06a2d9ec367ad0f510c775bfb0ef
Juhee0221 <jhk7764@gmail.com>
2024-04-15 17:48:30 +0900

    리뷰생성

--- src/main/java/com/green/StudyRoom/board/service/ReviewService.java
+++ src/main/java/com/green/StudyRoom/board/service/ReviewService.java
@@ -1,6 +1,7 @@
 package com.green.StudyRoom.board.service;
 
 import com.green.StudyRoom.board.vo.ReviewVO;
+import com.green.StudyRoom.member.vo.StudyRoomInOutVO;
 
 import java.util.List;
 
@@ -8,4 +9,8 @@ public interface ReviewService {
     void insertReview(ReviewVO reviewVO);
 
     List<ReviewVO> selectReview();
+
+    List<StudyRoomInOutVO> selectInout(int memberCode);
+
+    List<StudyRoomInOutVO> selectReviewPage();
 }
