8be3ae0c9a59de0d9f42c09715ed8ac94f9a1d35
SangHyeokY <ush529@gmail.com>
2024-05-03 14:45:36 +0900

    ysh0529

--- src/main/resources/static/js/admin/admin_log.js
+++ src/main/resources/static/js/admin/admin_log.js
@@ -4,14 +4,24 @@
 //     const currnetDate = document.querySelector('#currnetDate');
 
 //     var today = new Date();
-    
+
 //     var year = today.getFullYear();
 //     var month = ('0' + (today.getMonth() + 1)).slice(-2);
 //     var day = ('0' + today.getDate()).slice(-2);
-    
+
 //     var dateString = year + '-' + month  + '-' + day;
-    
+
 //     currnetDate.insertAdjacentHTML = dateString;
-    
+
 //     console.log(dateString);
+// }
+
+// numberComma()
+
+// function numberComma() {
+//   const commar = document.querySelectorAll('.comma').textContent;
+//   //setNumber = parseInt(commar.textContent).toLocaleString('ko-KR');
+//   commar.innerHTML = '';
+//   str += `${commar + '원'}`;
+//   commar.insertAdjacentHTML('afterbegin', str);
 // }
