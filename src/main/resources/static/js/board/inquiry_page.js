f3f6915d5fe5cf943243672554cbc08b7b016075
wltjsdud0782 <wltjsdud0782@gmail.com>
2024-05-16 14:37:49 +0900

    일부 수정

--- src/main/resources/static/js/board/inquiry_page.js
+++ src/main/resources/static/js/board/inquiry_page.js
@@ -7,3 +7,13 @@ function goDetailSelect(boardCode){
 function myWriting(){
     location.href="/board/myWriting"
 }
+
+function searchBtn(){
+    if (document.querySelector('.searchValue').value == '') {
+        alert('검색어를 입력해주세요.')
+    }
+    else{
+        const searchForm = document.querySelector('.searchForm');
+        searchForm.submit()
+    }
+}
