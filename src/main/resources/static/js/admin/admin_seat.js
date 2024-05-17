ca695dd13f77bd8ee59f54a5691bcb68b274e145
SangHyeokY <ush529@gmail.com>
2024-05-17 17:47:14 +0900

    ok

--- src/main/resources/static/js/admin/admin_seat.js
+++ src/main/resources/static/js/admin/admin_seat.js
@@ -22,8 +22,9 @@ function onefloor() {
         })
         //fetch 통신 후 실행 영역
         .then((data) => {//data -> controller에서 리턴되는 데이터!
+            console.log(data);
 
-            document.querySelector(".input-group").innerHTML = '';
+            document.querySelector(".group-button").innerHTML = '';
             let str_1 = '';
             str_1 += `
                 <button type="button" class="btn btn-light floor-btn" onclick="onefloor()">1층</button>
@@ -32,7 +33,7 @@ function onefloor() {
                 <button type="button" class="btn btn-outline-danger" onclick="moveMember()">회원이동</button>
                 <button type="button" class="btn btn-outline-danger" onclick="adminSeatOut()">회원퇴실</button>
             `;
-            document.querySelector(".input-group").insertAdjacentHTML('afterbegin', str_1);
+            document.querySelector(".group-button").insertAdjacentHTML('afterbegin', str_1);
 
             document.querySelector(".seatLive").innerHTML = '';
             let str = '';
@@ -955,4 +956,39 @@ function adminSeatOut() {
     else {
         return;
     }
+}
+
+function changeStatus(){
+    const num = document.querySelector('.statusNum').value;
+    alert(num);
+
+    fetch('/', { //요청경로
+        method: 'POST',
+        cache: 'no-cache',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+        },
+        //컨트롤러로 전달할 데이터
+        body: new URLSearchParams({
+           // 데이터명 : 데이터값
+        })
+    })
+    .then((response) => {
+        if(!response.ok){
+            alert('fetch error!\n컨트롤러로 통신중에 오류가 발생했습니다.');
+            return ;
+        }
+    
+        return response.text(); //컨트롤러에서 return하는 데이터가 없거나 int, String 일 때 사용
+        //return response.json(); //나머지 경우에 사용
+    })
+    //fetch 통신 후 실행 영역
+    .then((data) => {//data -> controller에서 리턴되는 데이터!
+        
+    })
+    //fetch 통신 실패 시 실행 영역
+    .catch(err=>{
+        alert('fetch error!\nthen 구문에서 오류가 발생했습니다.\n콘솔창을 확인하세요!');
+        console.log(err);
+    });
 }
